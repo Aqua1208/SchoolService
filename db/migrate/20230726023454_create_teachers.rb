8188f12f775dad8f9da810427d43141528d24324
Aqua1208 <mizusan1208@outlook.jp>
2023-07-26 11:44:22 +0900

    Lesson 機能に必要なModelの作成

---
+++ db/migrate/20230726023454_create_teachers.rb
@@ -0,0 +1,11 @@
+class CreateTeachers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :teachers do |t|
+      t.string :name
+      t.string :password_digest
+      t.string :comment
+
+      t.timestamps
+    end
+  end
+end
